e5111c14a5997be61f75ef795e9fc34e56f08ac1
machida <machida@kmc.gr.jp>
2019-10-08 23:50:03 +0900

    ログアウトできるようにした

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,16 @@ class SessionsController < ApplicationController
     user = User.find_by(email: session_params[:email])
     if user&.authenticate(session_params[:password])
       session[:user_id] = user.id
-      redirect_to root_url, notice: 'ログインしました'
+      redirect_to login_path, notice: 'ログインしました'
     else
       render :new
     end
   end
 
+  def destroy
+    reset_session
+    redirect_to login_path, notice: 'ログアウトしました'
+  end
   private
 
   def session_params
