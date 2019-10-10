7f8f7a01e1488b6c30b0519ac41f8b69d252fda2
machida <machida@kmc.gr.jp>
2019-10-11 01:10:38 +0900

    routesにべた書き

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
+  resources :topics do
+    resources :posts
+  end
 end
