6b2ae96db37b70d934753ca8cce05c466012f3a3
machida <machida@kmc.gr.jp>
2019-10-11 00:40:12 +0900

    トピックモデル作成&マイグレ

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_08_135924) do
+ActiveRecord::Schema.define(version: 2019_10_10_153830) do
+
+  create_table "topics", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
