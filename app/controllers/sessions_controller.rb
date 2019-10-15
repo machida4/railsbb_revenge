c24a8a9309075b059d812eea6c683560d1996e8e
machida <machida@kmc.gr.jp>
2019-10-15 20:37:13 +0900

    ログイン画面とログアウト画面分けた

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,14 @@ class SessionsController < ApplicationController
 
   def new
   end
+
   def create
     user = User.find_by(email: session_params[:email])
     if user&.authenticate(session_params[:password])
       session[:user_id] = user.id
-      redirect_to login_path, notice: 'ログインしました'
+      redirect_to topics_path, notice: 'ログインしました'
     else
+      flash[:alert] = "メールアドレスかパスワードが誤っています"
       render :new
     end
   end
@@ -17,6 +19,7 @@ class SessionsController < ApplicationController
     reset_session
     redirect_to login_path, notice: 'ログアウトしました'
   end
+
   private
 
   def session_params
