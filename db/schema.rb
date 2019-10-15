47d9c227c179d756f6bf97e3f0ad45f2798a28d4
machida <machida@kmc.gr.jp>
2019-10-15 22:55:57 +0900

    とりあえず導入完了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_10_154813) do
+ActiveRecord::Schema.define(version: 2019_10_15_134040) do
 
   create_table "posts", force: :cascade do |t|
     t.text "message"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2019_10_10_154813) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.text "image_data"
     t.index ["user_id"], name: "index_topics_on_user_id"
   end
 
